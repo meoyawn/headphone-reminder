494584c610537015251806e8f7a09b2fa5181933
adelnizamutdinov <stiggpwnz@gmail.com>
2015-08-07 20:19:25 +0300

    method names

--- app/src/androidTest/kotlin/adeln/boilerplate/AndroidTest.kt
+++ app/src/androidTest/kotlin/adeln/boilerplate/AndroidTest.kt
@@ -1,11 +1,9 @@
 package adeln.boilerplate
 
 import android.support.test.runner.AndroidJUnit4
-import android.test.suitebuilder.annotation.LargeTest
 import org.junit.Ignore
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
 @Ignore
 open class AndroidTest
--- app/src/androidTest/kotlin/adeln/boilerplate/TextViewTest.kt
+++ app/src/androidTest/kotlin/adeln/boilerplate/TextViewTest.kt
@@ -5,13 +5,15 @@ import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
+import android.test.suitebuilder.annotation.LargeTest
 import org.junit.Rule
 import org.junit.Test
 
+@LargeTest
 class TextViewTest : AndroidTest() {
   @Rule fun rule() = ActivityTestRule(javaClass<MainActivity>())
 
-  @Test fun testFail() {
+  @Test fun testText() {
     onView(withText("Fuck")).check(matches(isDisplayed()))
   }
 }
