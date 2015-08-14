022e06b52f4894b45c995e10bb924270c649a3a2
adelnizamutdinov <stiggpwnz@gmail.com>
2015-08-14 12:17:53 +0300

    fuck test inheritance

--- app/src/androidTest/kotlin/adeln/boilerplate/AndroidTest.kt
+++
@@ -1,9 +0,0 @@
-package adeln.boilerplate
-
-import android.support.test.runner.AndroidJUnit4
-import org.junit.Ignore
-import org.junit.runner.RunWith
-
-@RunWith(AndroidJUnit4::class)
-@Ignore
-open class AndroidTest
--- app/src/androidTest/kotlin/adeln/boilerplate/TextViewTest.kt
+++ app/src/androidTest/kotlin/adeln/boilerplate/TextViewTest.kt
@@ -4,12 +4,15 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.test.suitebuilder.annotation.LargeTest
+import android.support.test.runner.AndroidJUnit4
+import android.test.suitebuilder.annotation.SmallTest
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-@LargeTest
-class TextViewTest : AndroidTest() {
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class TextViewTest {
   @Rule fun rule() = NoAnimationsRule(javaClass<MainActivity>())
 
   @Test fun testText() {
