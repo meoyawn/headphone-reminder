9a452ccb5099932404a17da89819f5916ccd4afa
adelnizamutdinov <stiggpwnz@gmail.com>
2015-09-15 22:41:28 +0300

    unit tests

---
+++ app/src/test/kotlin/adeln/boilerplate/MainActivityTest.kt
@@ -0,0 +1,17 @@
+package adeln.boilerplate
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricGradleTestRunner
+
+@RunWith(RobolectricGradleTestRunner::class)
+class MainActivityTest {
+  @Test fun testPass() {
+    assertThat(2).isEqualTo(1 + 1)
+  }
+
+  @Test fun testFail() {
+    assertThat(false).isTrue()
+  }
+}
