1d8a5da3c97814d6badebd726dd198b9399426b4
adelnizamutdinov <stiggpwnz@gmail.com>
2015-10-25 02:40:16 +0300

    strict mode tweaks

--- app/src/main/kotlin/domain/Screen.kt
+++ app/src/main/kotlin/domain/Screen.kt
@@ -7,19 +7,19 @@ import common.view.byId
 import common.view.inflate
 import domain.record.audioRecord
 import domain.view.RecordView
+import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
-import rx.subjects.PublishSubject
 
 sealed class Screen {
   object Main : Screen() {
-    fun setup(root: ViewGroup, detaches: PublishSubject<View>): View = run {
+    fun setup(root: ViewGroup, detaches: Observable<View>): View = run {
       root.inflate(R.layout.activity_main)
     }
   }
 
   object Recorder : Screen() {
-    fun setup(root: ViewGroup, detaches: PublishSubject<View>): View = run {
+    fun setup(root: ViewGroup, detaches: Observable<View>): View = run {
       val v = root.inflate(R.layout.screen_recorder)
       val rv = v.byId<RecordView>(R.id.recorder)
 
